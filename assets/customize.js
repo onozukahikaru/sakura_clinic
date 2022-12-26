d9d2103d44e3f02a2ad6d5ea51d83f118f7ff6d4
onozukahikaru <62983870+onozukahikaru@users.noreply.github.com>
2022-12-26 15:36:49 +0900

    reviewのボタン

--- assets/customize.js
+++ assets/customize.js
@@ -1,12 +1,4 @@
 $(function () {
-  // ボタン1 (背景色を黄色に、文字サイズを12pxに指定）
-  $(".spr-summary-actions-newreview").click(function () {
-    console.log("click");
-    $(this).toggleClass(".on");
-  });
-});
-
-$(function () {
   $('a[href^="#"]').click(function () {
     var adjust = -100;
     var speed = 400;

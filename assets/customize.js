996459f6e5440d49976a493671d476698dfcedb3
onozukahikaru <62983870+onozukahikaru@users.noreply.github.com>
2022-12-26 19:20:44 +0900

    スライダー自動再生

--- assets/customize.js
+++ assets/customize.js
@@ -9,3 +9,11 @@ $(function () {
     return false;
   });
 });
+
+var links = document.links;
+for (let i = 0, linksLength = links.length; i < linksLength; i++) {
+  if (links[i].hostname !== window.location.hostname) {
+    links[i].target = "_blank";
+    links[i].rel = "noreferrer noopener";
+  }
+}
